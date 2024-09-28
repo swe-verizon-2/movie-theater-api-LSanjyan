c5c1295d5e7847282a6a571a7c9630514c7e0423
Lilit Sanjyan <133781980+LSanjyan@users.noreply.github.com>
2024-09-27 21:47:49 -0400

    Association added

--- src/routes/User.js
+++ src/routes/User.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../../models/User");
+const { User, Show } = require("../../models");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 
@@ -25,4 +25,26 @@ router.get("/:id", async (req, res, next) => {
     next(error);
   }
 });
+
+router.get("/:id/shows", async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const user = await User.findByPk(userId, {
+      include: Show,
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.json(user.Shows || []);
+  } catch (error) {
+    console.error("Error fetching shows:", error.message || error);
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+});
+
 module.exports = router;
